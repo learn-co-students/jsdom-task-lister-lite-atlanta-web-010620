f4d106da30eebacf7c7a6c1fbda45fa271f701e3
Dino <dinokunovac@gmail.com>
2020-02-26 09:13:21 -0500

    Done.

--- src/index.js
+++ src/index.js
@@ -1,3 +1,35 @@
 document.addEventListener("DOMContentLoaded", () => {
   // your code here
+  createNewTask();
+  
 });
+
+function createNewTask() {
+  const newForm = document.querySelector('#create-task-form');
+  const inputTask = document.querySelector('#new-task-description');
+  const taskUl = document.getElementById('tasks');
+  // const divList = document.getElementById('list');
+  
+  
+  newForm.addEventListener('submit', function(e) {
+    // debugger
+    e.preventDefault();
+    const newLi = document.createElement('li');
+    newLi.innerText = inputTask.value;
+    taskUl.appendChild(newLi);
+    
+    e.target.reset();
+    
+  })
+  
+}
+
+
+
+
+ 
+ 
+      
+ 
+
+      
