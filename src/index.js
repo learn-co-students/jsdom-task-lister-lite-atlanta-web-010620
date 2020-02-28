7adab59b5857023c9ffb5cabb5fe6378405654d7
Jacy Leech <jacy.atlanta@gmail.com>
2020-02-28 08:38:26 -0500

    Done.

--- src/index.js
+++ src/index.js
@@ -1,14 +1,16 @@
 document.addEventListener("DOMContentLoaded", () => {
 
-
-  let taskList = document.querySelector('#tasks')
-  const taskForm = document.querySelector('#create-task-form');
-  taskForm.addEventListener('submit', function (e) {
-    e.preventDefault();
-    let value = e.target.querySelector('#new-task-description').value;
-    let listItem = document.createElement('li')
-    listItem.innerText = value
-    taskList.appendChild(listItem)
+function myTasks() {
+  let createTasks = document.querySelector('#create_task_form')
+  createTasks.addEventListener('click', function(e) {
+  e.preventDefault()
+  let newTask = document.querySelector('#new_task_description')
+  let li = document.createElement('li')
+  li.innerText = newTask.value
+  let listTask = document.querySelector('#tasks')
+  listTask.appendChild(li)
   })
+}
+  
 
-});
+})
